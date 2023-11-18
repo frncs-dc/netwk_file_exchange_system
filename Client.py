4bf892cd9759eb21e0c3d9b8abaf9ea7c3617a15
frncs-dc <97236341+frncs-dc@users.noreply.github.com>
2023-11-18 11:08:08 +0800

    Update Client.py
    redid gui of client

--- Client.py
+++ Client.py
@@ -2,9 +2,22 @@ import tkinter as tk
 from tkinter import simpledialog
 
 ROOT = tk.Tk()
-ROOT.withdraw()
-# the input dialog
-USER_INP = simpledialog.askstring(title="Test",
-                                  prompt="What's your Name?:")
-# check it 
-print("Hello", USER_INP)
+ROOT.geometry("500x500")
+ROOT.title("Python File Sharing")
+
+labelCommand = tk.Label(ROOT, text="Input Command:", font=('Helvetica', 18))
+labelCommand.pack(padx=10, pady=10)
+
+# Inputs the Command Here
+textboxCommand = tk.Text(ROOT, height=1, font=('Helvetica', 18))
+textboxCommand.pack()
+
+# Clicks this to execute the command
+buttonCommand = tk.Button(ROOT, text="Enter", font=('Helvetica', 18))
+buttonCommand.pack(padx=10, pady=10)
+
+# Displays the error
+labelERROR = tk.Label(ROOT, text="Error:", font=('Helvetica', 14))
+labelERROR.pack()
+
+ROOT.mainloop()
